a1637c3aed83b5940b64e2cf714b9156d8d84193
Kevin O'Brien <kevin.obrien@lovehoney.co.uk>
2019-02-21 09:40:33 +0000

    Updating auth state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -18,13 +18,18 @@ componentDidMount() {
       this.auth = window.gapi.auth2.getAuthInstance();
       // Figure out if the user is currently signed in
       this.setState({ isSignedIn: this.auth.isSignedIn.get()} );
+      this.auth.isSignedIn.listen(this.onAuthChange);
     });
   });
 }
 
+onAuthChange = () => {
+  this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+};
+
 renderAuthButton() {
   if (this.state.isSignedIn === null) {
-    return <div>I don't know if we are signed in</div>
+    return <div>I don'/t know if we are signed in</div>
   } else if (this.state.isSignedIn) {
     return <div>I am signed in!</div>
   } else {
