2ae2f26ace5a33b10c7dd6adb4f3731ffdc12649
Kevin O'Brien <kevin.obrien@lovehoney.co.uk>
2019-02-22 14:58:11 +0000

    Made helper functions more descriptive of actual function

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -27,11 +27,11 @@ onAuthChange = () => {
   this.setState({ isSignedIn: this.auth.isSignedIn.get() });
 };
 
-onSignIn = () => {
+onSignInClick = () => {
   this.auth.signIn();
 };
 
-onSignOut = () => {
+onSignOutClick = () => {
   this.auth.signOut();
 };
 
@@ -40,14 +40,14 @@ renderAuthButton() {
     return null;
   } else if (this.state.isSignedIn) {
     return (
-      <button onClick={this.onSignOut} className="ui red google button">
+      <button onClick={this.onSignOutClick} className="ui red google button">
         <i className="google icon" />
         Sign Out
       </button>
     )
   } else {
     return (
-      <button onClick={this.onSignIn} className="ui red google button">
+      <button onClick={this.onSignInClick} className="ui red google button">
         <i className="google icon" />
         Sign In with Google
       </button>
